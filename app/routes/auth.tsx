9b91bf777d0f84349912d1ad84ba3b220340d9d0
Mahmud <mahxgithub@gmail.com>
2025-12-31 22:13:34 +0100

    feat: Introduce core UI components, data models, and Puter.js integration.

---
+++ app/routes/auth.tsx
@@ -0,0 +1,24 @@
+export const meta = () => (
+    [
+        {title: 'ResuMind | Auth'},
+        {name: 'description', content: 'Log into your account'}
+    ]
+)
+
+
+export default function Auth() {
+    return (
+        <div>
+            <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+                <div className="gradient-border shadow-lg">
+                    <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+                        <div className="flex flex-col items-center gap-2 text-center">
+                            <h1>Welcome</h1>
+                            <h2>Log In to Continue Your Job Journey</h2>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </div>
+    );
+}
